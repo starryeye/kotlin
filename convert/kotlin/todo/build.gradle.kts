b8ddcd3ac6d6688cced1a60af1b51956c7daf5ba
starryeye <see2818@naver.com>
2023-09-30 23:48:59 +0900

    [starryeye, 2023.09.30] - Java To Kotlin 설정 파일
    내용
    - build.gradle -> build.gradle.kts
    - settings.gradle -> setting.gradle.kts

--- convert/kotlin/todo/build.gradle.kts
+++ convert/kotlin/todo/build.gradle.kts
@@ -6,18 +6,22 @@ plugins {
     id ("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.spring") version "1.9.0"
+    // 코틀린은 기본적으로 final 클래스라 상속이 불가능함, 따라서 CGLIB 라이브러리를 통한 프록시 생성이 불가능하여 Open 키워드를 붙여준다.
+    // 대상: @Component, @Transactional 등
     kotlin("plugin.jpa") version "1.9.0"
+    // @Entity 의 경우 no argument constructor 가 필요하다. 이를 자동으로 생성해주는 플러그인
+    // 대상: @Entity, @Embeddable, @MappedSuperClass 등
 }
 
 group = "dev.practice"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
-configurations { // 사실 필요없음 롬복을 사용하지 않으므로..
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
+//configurations { // 사실 필요없음 롬복을 사용하지 않으므로..
+//    compileOnly {
+//        extendsFrom(configurations.annotationProcessor.get())
+//    }
+//}
 
 repositories {
     mavenCentral()
