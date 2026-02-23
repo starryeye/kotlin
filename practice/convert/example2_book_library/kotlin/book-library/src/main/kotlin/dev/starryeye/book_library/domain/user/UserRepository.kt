1fa8b00188a9e21af29f11418c5e0ef093d9e768
starryeye <see2818@naver.com>
2026-02-23 22:31:23 +0900

    [starryeye, 2026.02.23] - kotlin review
    내용
    - kotlin review

---
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/domain/user/UserRepository.kt
@@ -0,0 +1,20 @@
+package dev.starryeye.book_library.domain.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface UserRepository : JpaRepository<User, Long> {
+
+    @Query(
+        """
+        select u
+        from User u
+        left join fetch u.loanHistories
+        where u.id = :userId
+        """
+    )
+    fun findByIdWithLoanHistories(
+        @Param("userId") userId: Long
+    ): User?
+}
---
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/domain/user/loan_history/UserLoanHistoryRepository.kt
@@ -0,0 +1,8 @@
+package dev.starryeye.book_library.domain.user.loan_history
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
+
+    fun existsByBookIdAndIsReturn(bookId: Long, isReturn: Boolean): Boolean
+}
