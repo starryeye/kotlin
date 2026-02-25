f1a2776fd0acbd10c87429eb41190b64f121d101
starryeye <see2818@naver.com>
2026-02-25 19:07:32 +0900

    [starryeye, 2026.02.25] - kotlin review
    내용
    - kotlin review

--- practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/application/user/command/ChangeUsernameService.kt
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/application/user/command/ChangeUsernameService.kt
@@ -1,6 +1,9 @@
 package dev.starryeye.book_library.application.user.command
 
+import dev.starryeye.book_library.application.user.command.command.ChangeUsernameCommand
 import dev.starryeye.book_library.domain.user.UserRepository
+import dev.starryeye.book_library.util.fail
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -9,4 +12,12 @@ import org.springframework.transaction.annotation.Transactional
 class ChangeUsernameService(
     private val userRepository: UserRepository,
 ) {
+
+    fun changeUsername(command: ChangeUsernameCommand) {
+
+        val user = userRepository.findByIdOrNull(command.id)
+            ?: fail("user is not found, id = " + command.id)
+
+        user.changeUsername(command.username)
+    }
 }
--- practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/application/user/command/RegisterUserService.kt
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/application/user/command/RegisterUserService.kt
@@ -1,5 +1,8 @@
 package dev.starryeye.book_library.application.user.command
 
+import dev.starryeye.book_library.application.user.command.command.RegisterUserCommand
+import dev.starryeye.book_library.application.user.command.result.RegisterUserResult
+import dev.starryeye.book_library.domain.user.User
 import dev.starryeye.book_library.domain.user.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -9,4 +12,16 @@ import org.springframework.transaction.annotation.Transactional
 class RegisterUserService(
     private val userRepository: UserRepository,
 ) {
+
+    fun register(command: RegisterUserCommand): RegisterUserResult {
+
+        val user = User(
+            username = command.username,
+            age = command.age,
+        )
+
+        userRepository.save(user)
+
+        return RegisterUserResult.of(user)
+    }
 }
