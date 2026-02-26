d72c3cc1ac0f98a954bc37912396114635f0d1f3
starryeye <see2818@naver.com>
2026-02-26 23:39:34 +0900

    [starryeye, 2026.02.26] - kotlin review
    내용
    - kotlin review

--- practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/request/ChangeUsernameRequest.kt
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/request/ChangeUsernameRequest.kt
@@ -1,4 +1,19 @@
 package dev.starryeye.book_library.controller.user.request
 
-class ChangeUsernameRequest {
+import dev.starryeye.book_library.application.user.command.command.ChangeUsernameCommand
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+data class ChangeUsernameRequest(
+    @field:NotNull
+    val id: Long?,
+    @field:NotBlank
+    val username: String?
+)
+
+fun ChangeUsernameRequest.toCommand(): ChangeUsernameCommand {
+    return ChangeUsernameCommand(
+        id = id!!,
+        username = this.username!!,
+    )
 }
--- practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/request/RegisterUserRequest.kt
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/request/RegisterUserRequest.kt
@@ -1,4 +1,17 @@
 package dev.starryeye.book_library.controller.user.request
 
-class RegisterUserRequest {
+import dev.starryeye.book_library.application.user.command.command.RegisterUserCommand
+import jakarta.validation.constraints.NotBlank
+
+class RegisterUserRequest(
+    @field:NotBlank
+    val username: String?,
+    val age: Int?,
+)
+
+fun RegisterUserRequest.toCommand(): RegisterUserCommand {
+    return RegisterUserCommand(
+        username = this.username!!,
+        age = this.age
+    )
 }
