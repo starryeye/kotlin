c7cafb7d346fc7581f9bec37be28e56aa592f4a7
starryeye <see2818@naver.com>
2026-02-27 21:30:00 +0900

    kotlin review

--- practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/response/UserResponse.kt
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/response/UserResponse.kt
@@ -1,4 +1,20 @@
 package dev.starryeye.book_library.controller.user.response
 
-class UserResponse {
+import dev.starryeye.book_library.application.user.command.result.RegisterUserResult
+
+data class UserResponse(
+    val id: Long,
+    val username: String,
+    val age: Int?,
+) {
+
+    companion object {
+        fun of(userResult: RegisterUserResult): UserResponse {
+            return UserResponse(
+                id = userResult.id,
+                username = userResult.username,
+                age = userResult.age,
+            )
+        }
+    }
 }
--- practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/response/UsersResponse.kt
+++ practice/convert/example2_book_library/kotlin/book-library/src/main/kotlin/dev/starryeye/book_library/controller/user/response/UsersResponse.kt
@@ -1,4 +1,20 @@
 package dev.starryeye.book_library.controller.user.response
 
-class UsersResponse {
+import dev.starryeye.book_library.application.user.query.result.GetUsersResult
+
+data class UsersResponse(
+    val users: List<UserResponse>,
+) {
+
+    companion object {
+        fun of(usersResult: GetUsersResult): UsersResponse {
+            return UsersResponse(
+                usersResult.users.map { userResult -> UserResponse(
+                    userResult.id,
+                    userResult.username,
+                    userResult.age
+                ) }
+            )
+        }
+    }
 }
