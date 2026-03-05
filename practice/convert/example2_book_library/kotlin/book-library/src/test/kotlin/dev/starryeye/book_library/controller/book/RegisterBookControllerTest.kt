163a09af953e62d9e95ca82c6391003acf4666ca
starryeye <see2818@naver.com>
2026-03-06 01:03:16 +0900

    [starryeye, 2026.03.06] - kotlin review
    내용
    - kotlin review

--- practice/convert/example2_book_library/kotlin/book-library/src/test/kotlin/dev/starryeye/book_library/controller/book/RegisterBookControllerTest.kt
+++ practice/convert/example2_book_library/kotlin/book-library/src/test/kotlin/dev/starryeye/book_library/controller/book/RegisterBookControllerTest.kt
@@ -25,7 +25,7 @@ class RegisterBookControllerTest @Autowired constructor(
 
     @DisplayName("POST /api/v1/books/new 요청으로 책을 등록한다.")
     @Test
-    fun registerBook() {
+    fun registerBook1() {
 
         // given
         val requestBody = mapOf(
@@ -42,5 +42,87 @@ class RegisterBookControllerTest @Autowired constructor(
         )
             .andDo(print())
             .andExpect(status().isOk)
+    }
+
+    @DisplayName("책 이름은 필수인데 없으면 400 에러")
+    @Test
+    fun registerBook2() {
+
+        // given
+        val requestBody = mapOf(
+            "category" to BookCategory.SCIENCE.name,
+        )
+
+        // when
+        // then
+        mockMvc.perform(
+            post("/api/v1/books/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestBody))
+        )
+            .andDo(print())
+            .andExpect(status().isBadRequest)
+    }
+
+    @DisplayName("책 이름이 blank 이면 400 에러")
+    @Test
+    fun registerBook3() {
+
+        // given
+        val requestBody = mapOf(
+            "bookname" to " ",
+            "category" to BookCategory.SCIENCE.name,
+        )
+
+        // when
+        // then
+        mockMvc.perform(
+            post("/api/v1/books/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestBody))
+        )
+            .andDo(print())
+            .andExpect(status().isBadRequest)
+    }
+
+    @DisplayName("책 카테고리는 필수인데 없으면 400 에러")
+    @Test
+    fun registerBook4() {
+
+        // given
+        val requestBody = mapOf(
+            "bookname" to "book name 1",
+        )
+
+        // when
+        // then
+        mockMvc.perform(
+            post("/api/v1/books/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestBody))
+        )
+            .andDo(print())
+            .andExpect(status().isBadRequest)
+    }
+
+    @DisplayName("책 카테고리가 BookCategory enum 값이 아니면 400 에러")
+    @Test
+    fun registerBook5() {
+
+        // given
+        val requestBody = mapOf(
+            "bookname" to "book name 1",
+            "category" to "XXX",
+        )
+
+        // when
+        // then
+        mockMvc.perform(
+            post("/api/v1/books/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestBody))
+        )
+            .andDo(print())
+            .andExpect(status().isBadRequest)
     }
 }
