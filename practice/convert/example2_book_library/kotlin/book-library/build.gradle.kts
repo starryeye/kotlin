50bb2706775c9f754db96a4d36c697a3010c34fa
starryeye <see2818@naver.com>
2026-03-10 23:40:24 +0900

    [starryeye, 2026.03.10] - kotlin review
    내용
    - kotlin review

--- practice/convert/example2_book_library/kotlin/book-library/build.gradle.kts
+++ practice/convert/example2_book_library/kotlin/book-library/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "4.0.3"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "2.2.21" // entity no-argument constructor 없어도 해결
+	kotlin("kapt") version "2.2.21" // querydsl
 }
 
 group = "dev.starryeye"
@@ -27,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect") // 코틀린 객체 reflection 가능하도록 해줌
 	implementation("tools.jackson.module:jackson-module-kotlin") // 코틀린 dto serialize, deserialize 지원
+	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
 
 	runtimeOnly("com.h2database:h2")
 
@@ -34,10 +36,14 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("io.mockk:mockk:1.13.11")
-	testImplementation("com.ninja-squad:springmockk:4.0.2")
+	testImplementation("io.mockk:mockk-jvm:1.14.9")
+	testImplementation("com.ninja-squad:springmockk:5.0.1")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+	kapt("jakarta.annotation:jakarta.annotation-api")
+	kapt("jakarta.persistence:jakarta.persistence-api")
 }
 
 kotlin {
@@ -50,6 +56,10 @@ allOpen {
 	annotation("jakarta.persistence.Entity")
 	annotation("jakarta.persistence.MappedSuperclass")
 	annotation("jakarta.persistence.Embeddable")
+}
+
+kapt {
+	correctErrorTypes = true
 }
 
 tasks.withType<Test> {
